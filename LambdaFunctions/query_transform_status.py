0218aeb6ed76f712b0e0e25120afb4eb1a7a0a60
Wilkinson <swlkn@amazon.com>
2019-04-05 09:30:56 -0700

    Small change

--- LambdaFunctions/query_transform_status.py
+++ LambdaFunctions/query_transform_status.py
@@ -17,13 +17,13 @@ def lambda_handler(event, context):
         #Transform job name follows 'transform_job/' string in ARN
         job_name = transform_arn.rsplit(sep='transform-job/', maxsplit=1)[-1]
     else:
-        raise KeyError('TransformJobArn key not found in input! Input to Step' +
-            ' Function must include JobName!')
+        raise KeyError('TransformJobArn key not found in input! Input' +
+            ' must include TransformJobArn!')
 
     #Query boto3 API to check transform status.
     try:
         response = sm_client.describe_transform_job(TransformJobName=job_name)
-        logger.info("Transform job:{} has status:{}!".format(job_name,
+        logger.info("Transform job:{} has status:{}.".format(job_name,
             response['TransformJobStatus']))
 
     except Exception as e:
